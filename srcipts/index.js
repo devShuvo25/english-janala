63e41a94f5c2919e99ecb09ed21b7f56fdbe674a
Shuvo Mallik <shuvomallik999@gmail.com>
2025-09-05 16:24:04 +0600

    mobile view issu solved

--- srcipts/index.js
+++ srcipts/index.js
@@ -15,7 +15,7 @@ lebelContainer.innerText = '';
        //     1-> create element
     const btnDiv = document.createElement('div');
     btnDiv.innerHTML = `
-    <button onclick="loadWords(${lesson.level_no})" class="hidden lg:block btn btn-outline btn-primary"><i class="fa-solid fa-circle-question"></i> Lesson-${lesson.level_no}</button>
+    <button onclick="loadWords(${lesson.level_no})" class=" btn btn-outline btn-primary"><i class="fa-solid fa-circle-question"></i> Lesson-${lesson.level_no}</button>
     `;
      //   2-> append */
      lebelContainer.append(btnDiv);
@@ -31,10 +31,12 @@ fetch(url)
 const displayWords = (words) => {
     const wordCardContainer = document.getElementById('word-card-container');
     wordCardContainer.innerText = '';
-    words.forEach(word => {
+    const card = document.createElement('div');
+    if(words.length !== 0){
+            words.forEach(word => {
         const card = document.createElement('div');
-        card.innerText = 'shuvo';
-        card.innerHTML = `<div class="lg:m-5 lg:px-5 w-full h-[180px] lg:w-[250px] lg:h-[180px] bg-white rounded-[4px] p-5 space-y-3 space-x-2">
+            card.classList.add('w-[100%]')
+             card.innerHTML = `<div class="lg:m-5 lg:px-5 w-[100%] bg-white rounded-[4px] p-5 space-y-3 space-x-2">
             <div class="text-center p-3">
             <h4 class="font-bold text-[18px] ">${word.word}</h4>
             <p class="text-[8px] my-2 text-gray-600">${word.pronunciation}.</p>
@@ -45,9 +47,23 @@ const displayWords = (words) => {
                 <i class="fa-solid fa-volume-high bg-[#9cddf4] p-1.5 text-[12px] rounded-[4px]"></i>
             </div>
         </div>`
-        wordCardContainer.append(card);
-        
+        wordCardContainer.append(card); 
     });
-    
+        
+    }
+    else{
+        wordCardContainer.classList.remove('h-[400px]');
+        card.classList.add('col-span-4');
+        card.innerHTML = `
+                <div class="text-center col-span-4 p-5">
+                <i class="fa-solid fa-triangle-exclamation text-[30px]"></i>
+        <p class="text-[10px] text-gray-500 my-3">এই Lesson এ কোনো Vocabullery যুক্ত করা হয়নি</p>
+        <h3 class="text-lg font-bold">পরবর্তী Lesson এ যান</h3>
+        </div>
+        `
+        wordCardContainer.append(card);
+    }
+
+
 
 }
